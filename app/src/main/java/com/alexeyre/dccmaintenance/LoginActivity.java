35dfd80f5c52c8e5b75764b549477fcb7e8eb597
AlexE0697 <eyrealex97@gmail.com>
2020-12-16 22:01:54 +0000

    fixing UI, cleaning login code

--- app/src/main/java/com/alexeyre/dccmaintenance/LoginActivity.java
+++ app/src/main/java/com/alexeyre/dccmaintenance/LoginActivity.java
@@ -19,6 +19,7 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
 
+import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -52,28 +53,36 @@ public class LoginActivity extends AppCompatActivity {
         loginBtn = findViewById(R.id.login_btn);
         accountBtn = findViewById(R.id.login_reg_btn);
 
-        //transfer to the login page
-        accountBtn.setOnClickListener(new View.OnClickListener() {
+        //initialize functionality
+        initComponents();
+
+
+    }
+
+    private void initComponents() {
+        //attempt to login
+        findViewById(R.id.login_btn).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
-
-                Pair[] pairs = new Pair[7];
-                pairs[0] = new Pair<View, String>(image, "app_image");
-                pairs[1] = new Pair<View, String>(logo, "app_text");
-                pairs[2] = new Pair<View, String>(desc, "desc_tran");
-                pairs[3] = new Pair<View, String>(loginUsername, "username_tran");
-                pairs[4] = new Pair<View, String>(loginPassword, "password_tran");
-                pairs[5] = new Pair<View, String>(loginBtn, "btn_tran");
-                pairs[6] = new Pair<View, String>(accountBtn, "account_tran");
-
-                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
-                    ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(LoginActivity.this, pairs);
-                    startActivity(intent, options.toBundle());
-                }
+                String username = ((TextInputEditText) findViewById(R.id.login_username)).getText().toString();
+                String password = ((TextInputEditText) findViewById(R.id.login_password)).getText().toString();
+                attemptLogin(username, password);
             }
         });
 
+    }
+
+    private void attemptLogin(String username, String password) {
+        ((TextInputEditText) findViewById(R.id.login_username)).setError(null);
+        ((TextInputEditText) findViewById(R.id.login_password)).setError(null);
+        if (username.equals("")) {
+            ((TextInputEditText) findViewById(R.id.login_username)).setError("Username Required");
+            return;
+        }
+        if (password.equals("")) {
+            ((TextInputEditText) findViewById(R.id.login_password)).setError("Password Required");
+            return;
+        }
     }
 
     private Boolean validateUsername() {
@@ -142,6 +151,28 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
     }//end isUser class
+
+    //Call Register Activity
+    public void callRegisterActivity(View view) {
+        //To call next activity
+        Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+
+        //create pairs for animation
+        Pair[] pairs = new Pair[7];
+        pairs[0] = new Pair<View, String>(image, "app_image");
+        pairs[1] = new Pair<View, String>(logo, "app_text");
+        pairs[2] = new Pair<View, String>(desc, "desc_tran");
+        pairs[3] = new Pair<View, String>(loginUsername, "username_tran");
+        pairs[4] = new Pair<View, String>(loginPassword, "password_tran");
+        pairs[5] = new Pair<View, String>(loginBtn, "btn_tran");
+        pairs[6] = new Pair<View, String>(accountBtn, "account_tran");
+
+        //Call next activity by attaching the animation with it.
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+            ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(LoginActivity.this, pairs);
+            startActivity(intent, options.toBundle());
+        }
+    }//end callRegisterActivity
 
     //when the back button is pressed
     @Override
