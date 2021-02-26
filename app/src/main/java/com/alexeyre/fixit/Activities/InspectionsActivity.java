2c9f212831af83263f2dc9bf3a0ad20cbcd03c51
Alex Eyre <eyrealex97@gmail.com>
2021-02-26 18:15:54 +0000

    create adapter for inspection report lists, set up all inspections activity, needs to be refined. Currently shows all traffic lights in database, need to make it show all inspection reports

--- app/src/main/java/com/alexeyre/fixit/Activities/InspectionsActivity.java
+++ app/src/main/java/com/alexeyre/fixit/Activities/InspectionsActivity.java
@@ -1,19 +1,78 @@
 package com.alexeyre.fixit.Activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.alexeyre.fixit.Adapters.ReportListAdapter;
+import com.alexeyre.fixit.Constants.Constants;
+import com.alexeyre.fixit.Helpers.TrafficLightModel;
 import com.alexeyre.fixit.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 
 public class InspectionsActivity extends AppCompatActivity {
+
+    //variables
+    private ArrayList<TrafficLightModel> trafficLightModels;
+    private ReportListAdapter reportListAdapter;
+    private RecyclerView recyclerView;
+
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inspections);
+
+        //create hooks
+        recyclerView = (RecyclerView) findViewById(R.id.inspection_report_recycler_view);
+
+        //set recycle view of list in a linear fashion
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(InspectionsActivity.this);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        FirebaseDatabase.getInstance().getReference().child(Constants.COORDINATES).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                trafficLightModels = new ArrayList<>();
+
+                for (DataSnapshot trafficLightDataSnapthot : snapshot.getChildren()) {
+                    if (trafficLightDataSnapthot != null && trafficLightDataSnapthot.hasChildren()) {
+                        TrafficLightModel trafficLightModel = trafficLightDataSnapthot.getValue(TrafficLightModel.class);
+                        trafficLightModel.setkey(trafficLightDataSnapthot.getKey());
+                        trafficLightModels.add(trafficLightModel);
+                    }
+                }
+                reportListAdapter = new ReportListAdapter(InspectionsActivity.this, trafficLightModels);
+                recyclerView.setAdapter(reportListAdapter);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Log.d("FIREBASE", "onCancelled: " + error.toString());
+            }
+        });
+
+
+    }//end on create
+
+    @Override
+    public void onBackPressed() {
+        startActivity(new Intent(InspectionsActivity.this, MainActivity.class));
+        finish();
+        overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
     }
 
 
-}
+}//end class
---
+++ app/src/main/java/com/alexeyre/fixit/Adapters/ReportListAdapter.java
@@ -0,0 +1,97 @@
+package com.alexeyre.fixit.Adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.ScaleAnimation;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.alexeyre.fixit.Helpers.TrafficLightModel;
+import com.alexeyre.fixit.R;
+
+import java.util.ArrayList;
+import java.util.Locale;
+
+public class ReportListAdapter extends RecyclerView.Adapter<ReportListViewHolder> {
+
+    //variables
+    private Context context;
+    private ArrayList<TrafficLightModel> trafficLightModels = new ArrayList<>();
+    private int lastPosition = -1;
+
+    public ReportListAdapter(Context context, ArrayList<TrafficLightModel> trafficLightModels) {
+        this.context = context;
+        this.trafficLightModels = trafficLightModels;
+    }
+
+
+    @NonNull
+    @Override
+    public ReportListViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.report_list_item, viewGroup, false);
+        return new ReportListViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ReportListViewHolder reportListViewHolder, int position) {
+
+        //set the template lists with data using ID and Location from TrafficLightModel
+        reportListViewHolder.id_tv.setText(String.format(Locale.ENGLISH, "ID: %s", trafficLightModels.get(position).getkey()).trim());
+        reportListViewHolder.location_tv.setText(String.format(Locale.ENGLISH, "Location: %s", trafficLightModels.get(position).getname()).trim());
+
+        //set the template lists with data when the inspection was reported from TrafficLightInspectionModel
+        //if there has been no inspections previously reported, print out N/A
+
+        if (trafficLightModels.get(position) == null) {
+            reportListViewHolder.reported_on_tv.setText(String.format(Locale.ENGLISH, "Reported on: N/A"));
+        } else {
+           reportListViewHolder.reported_on_tv.setText(String.format(Locale.ENGLISH, "Reported on: %s", trafficLightModels.get(position).gettimestamp()).trim());
+        }
+
+        //create onclick for each list item
+        //pass bundle info from list item to the next activity
+
+    }
+
+    private void setAnimation(View viewToAnimate, int position) {
+        if (position > lastPosition) {
+
+            ScaleAnimation animation = new ScaleAnimation(0.0f, 1.0f, 0.0f, 1.0f,
+                    Animation.RELATIVE_TO_SELF, 0.5f,
+                    Animation.RELATIVE_TO_SELF, 0.5f);
+            animation.setDuration(1500);
+            viewToAnimate.startAnimation(animation);
+            lastPosition = position;
+
+
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return trafficLightModels.size();
+    }
+}
+
+class ReportListViewHolder extends RecyclerView.ViewHolder {
+    TextView id_tv, location_tv, reported_on_tv;
+    CardView parent;
+
+
+    public ReportListViewHolder(@NonNull View itemView) {
+        super(itemView);
+
+        id_tv = itemView.findViewById(R.id.id_tv);
+        location_tv = itemView.findViewById(R.id.location_tv);
+        reported_on_tv = itemView.findViewById(R.id.reported_on_tv);
+        parent = itemView.findViewById(R.id.parent_cv);
+    }
+
+
+}
