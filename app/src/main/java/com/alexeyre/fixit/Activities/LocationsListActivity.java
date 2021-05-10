363ad8e290d5017bc5204110d6447213bba22fa5
Alex Eyre <eyrealex97@gmail.com>
2021-05-10 03:54:43 +0100

    added search function to locations activity

--- app/src/main/java/com/alexeyre/fixit/Activities/LocationsListActivity.java
+++ app/src/main/java/com/alexeyre/fixit/Activities/LocationsListActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.SearchView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,11 +12,13 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.alexeyre.fixit.Adapters.LocationsListAdapter;
+import com.alexeyre.fixit.Adapters.ReportListAdapter;
 import com.alexeyre.fixit.Constants.Constants;
 import com.alexeyre.fixit.Models.TrafficLightModel;
 import com.alexeyre.fixit.R;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
@@ -24,9 +27,9 @@ import java.util.ArrayList;
 public class LocationsListActivity extends AppCompatActivity {
 
     //variables
-    private ArrayList<TrafficLightModel> mInspectionTemplateList;
-    private LocationsListAdapter locationsListAdapter;
-    private RecyclerView mRecyclerView;
+    private ArrayList<TrafficLightModel> trafficLightModels;
+    private SearchView searchView;
+    private DatabaseReference databaseReference =  FirebaseDatabase.getInstance().getReference().child(Constants.COORDINATES);
 
 
     @Override
@@ -34,43 +37,80 @@ public class LocationsListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_locations_list);
 
-        mRecyclerView = (RecyclerView) findViewById(R.id.job_inspections_recycler_view);
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(LocationsListActivity.this);
-        mRecyclerView.setLayoutManager(linearLayoutManager);
+        //hooks
+        searchView = findViewById(R.id.search_field_locations);
+        searchView.setQueryHint("Search by id ...");
+
+        if(databaseReference != null){
+            //reference the database path for the coordinates
+            databaseReference.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    trafficLightModels = new ArrayList<>();
+
+                    for (DataSnapshot trafficLightDataSnapshot : snapshot.getChildren()) {
+                        try{
+                            if (trafficLightDataSnapshot != null && trafficLightDataSnapshot.hasChildren()) {
+                                TrafficLightModel trafficLightModel = trafficLightDataSnapshot.getValue(TrafficLightModel.class);
+                                trafficLightModel.setkey(trafficLightDataSnapshot.getKey());//We can assume all data is present
+                                trafficLightModels.add(trafficLightModel);
 
 
-        //reference the database path for the coordinates
-        FirebaseDatabase.getInstance().getReference().child(Constants.COORDINATES).addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                mInspectionTemplateList = new ArrayList<>();
-
-                for (DataSnapshot trafficLightDataSnapshot : snapshot.getChildren()) {
-                    try{
-                        if (trafficLightDataSnapshot != null && trafficLightDataSnapshot.hasChildren()) {
-                            TrafficLightModel trafficLightModel = trafficLightDataSnapshot.getValue(TrafficLightModel.class);
-                            trafficLightModel.setkey(trafficLightDataSnapshot.getKey());//We can assume all data is present
-                            mInspectionTemplateList.add(trafficLightModel);
-
+                            }
+                        }catch (Exception e){
 
                         }
-                    }catch (Exception e){
 
                     }
+                    setAdapter(trafficLightModels);
 
                 }
-                locationsListAdapter = new LocationsListAdapter(LocationsListActivity.this, mInspectionTemplateList);
-                mRecyclerView.setAdapter(locationsListAdapter);
 
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+                    Log.d("FIREBASE", "onCancelled: " + error.toString());
+                }
+
+            });
+        }if(searchView != null){
+            searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+                @Override
+                public boolean onQueryTextSubmit(String s) {
+                    return false;
+                }
+
+                @Override
+                public boolean onQueryTextChange(String s) {
+                    search(s);
+                    return true;
+                }
+            });
+        }
+
+
+    }
+
+    private void search(String str) {
+        ArrayList<TrafficLightModel> list = new ArrayList<>();
+        for (TrafficLightModel object : trafficLightModels) {
+            if (object.getkey().toLowerCase().contains(str.toLowerCase())) {
+                list.add(object);
             }
+        }
 
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-                Log.d("FIREBASE", "onCancelled: " + error.toString());
-            }
+        setSearchAdapter(list);
+    }
 
-        });
+    private void setSearchAdapter(ArrayList<TrafficLightModel> list) {
+        RecyclerView recyclerView = findViewById(R.id.job_inspections_recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(new LocationsListAdapter(this, list));
+    }
 
+    private void setAdapter(ArrayList<TrafficLightModel> trafficLightModels) {
+        RecyclerView recyclerView = findViewById(R.id.job_inspections_recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(new LocationsListAdapter(this, trafficLightModels));
     }
 
     @Override
